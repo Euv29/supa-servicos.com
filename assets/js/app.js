770378744dceb57d6e89b1ae7b6248f06221bff8
Venâncio Wapinda <euvenancio1@gmail.com>
2024-05-09 00:53:13 +0100

    Adicionei a estrutura de diretorios, uma animação de typing na heroSection e animação de linkCursor

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+// // Seleciona as imagens
+// const images = document.querySelectorAll('.service-img img');
+// let currentIndex = 0;
+
+// // Função para alternar as imagens
+// function changeImage() {
+//     // Oculta a imagem atual
+//     images[currentIndex].style.opacity = '0';
+    
+//     // Atualiza o índice para a próxima imagem
+//     currentIndex = (currentIndex + 1) % images.length;
+    
+//     // Mostra a próxima imagem
+//     images[currentIndex].style.opacity = '1';
+// }
+
+// // Define um intervalo para chamar a função de mudança de imagem a cada 3 segundos (3000 milissegundos)
+// setInterval(changeImage, 2000);
